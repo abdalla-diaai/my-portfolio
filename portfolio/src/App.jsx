d967cd5154bf9a74c8f99c293328801564ed90f7
Abdalla Diaai <56742529+abdalla-diaai@users.noreply.github.com>
2024-03-02 20:42:27 +0000

    added some context to home page.

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,32 +1,27 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Contact from './components/pages/Contact'
-import Header from './components/Header'
-import Home from './components/pages/Home'
-import Project from './components/pages/Project'
-import ProjectGallery from './components/pages/ProjectGallery'
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Contact from "./components/pages/Contact";
+import Header from "./components/Header";
+import Home from "./components/pages/Home";
+import Project from "./components/pages/Project";
+import ProjectGallery from "./components/pages/ProjectGallery";
 
 export default function App() {
   return (
     <>
-       <Router>
-       <Header />
-      {/* Wrap Route elements in a Routes component */}
-      <Routes>
-        {/* Define routes using the Route component to render different page components at different paths */}
-        {/* Define a default route that will render the Home component */}
-        <Route path="/" element={<Home />} />
-        <Route path="project" element={<Project />} />
-        <Route path="projectGallery" element={<ProjectGallery />} />
-        {/* Define a route that will have descendant routes */}
-        <Route path="contact" element={<Contact />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Header />
+        {/* Wrap Route elements in a Routes component */}
+        <Routes>
+          {/* Define routes using the Route component to render different page components at different paths */}
+          {/* Define a default route that will render the Home component */}
+          <Route path="/" element={<Home />} />
+          <Route path="project" element={<Project />} />
+          <Route path="projectGallery" element={<ProjectGallery />} />
+          {/* Define a route that will have descendant routes */}
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
-
