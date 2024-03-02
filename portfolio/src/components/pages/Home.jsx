68863832e1f12dcd276d816931331d86b34756d2
Abdalla Diaai <56742529+abdalla-diaai@users.noreply.github.com>
2024-03-02 21:22:32 +0000

    added contact form in contact page.

--- portfolio/src/components/pages/Home.jsx
+++ portfolio/src/components/pages/Home.jsx
@@ -6,17 +6,25 @@ export default function Home() {
     <Container>
       <h1 className="display-4">Hi there, I am Abdalla Diaai!</h1>
       <p className="lead">
-        I am passionate about solving problems that face scientists in the laboratory through developing user friendly tools that minize waste and improve efficiency. 
+        I am passionate about solving problems that face scientists in scientific research through developing user friendly tools that minize waste and
+        improve efficiency.
       </p>
       <p className="lead">
-        <a className="btn btn-primary btn-sm" href="#" role="button">
-        <i class="fa-brands fa-linkedin-in"></i>
+        <a
+          className="btn btn-primary btn-sm"
+          href="https://www.linkedin.com/in/abdalla-diaai-mohamed-434096218/"
+          role="button"
+        >
+          <i class="fa-brands fa-linkedin-in"></i>
         </a>
-        <a className="btn btn-dark btn-sm" href="#" role="button">
-        <i class="fa-brands fa-github"></i>
+        <a
+          className="btn btn-dark btn-sm"
+          href="https://github.com/abdalla-diaai"
+          role="button"
+        >
+          <i class="fa-brands fa-github"></i>
         </a>
       </p>
-      
     </Container>
   );
 }
