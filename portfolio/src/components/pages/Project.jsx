c0074382170bfe2b4874f5c90ed5711e6baeee3c
Abdalla Diaai <56742529+abdalla-diaai@users.noreply.github.com>
2024-03-03 15:02:50 +0000

    added gif and screenshots of some projects.

--- portfolio/src/components/pages/Project.jsx
+++ portfolio/src/components/pages/Project.jsx
@@ -3,31 +3,27 @@ import React from "react";
 export default function Project(props) {
   return (
     <div className="card">
-      <div className="img-container">
-        <img alt={props.name} src={props.image} />
-      </div>
-      <div className="content">
-        <ul>
-          <li>
-            <strong>Project Title:</strong> {props.title}
-          </li>
-          <li>
-            <a
-              className="btn btn-dark btn-sm home-btn"
-              href={props.repoUrl}
-              role="button"
-            >
-              GitHub Repository
-            </a>
-            <a
-              className="btn btn-dark btn-sm home-btn"
-              href={props.pageUrl}
-              role="button"
-            >
-              {props.title} Page
-            </a>
-          </li>
-        </ul>
+      <img className="project-image" alt={props.name} src={props.image} />
+      <div className="card-body">
+        <h5 clasName="card-title">{props.title}</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <a
+          className="btn btn-dark btn-sm home-btn"
+          href={props.repoUrl}
+          role="button"
+        >
+          GitHub Repository
+        </a>
+        <a
+          className="btn btn-dark btn-sm home-btn"
+          href={props.pageUrl}
+          role="button"
+        >
+          {props.title} Page
+        </a>
       </div>
     </div>
   );
--- portfolio/src/components/pages/ProjectGallery.jsx
+++ portfolio/src/components/pages/ProjectGallery.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import Project from "./Project";
-import projects from '../utils/projects.json'
+import projects from "../utils/projects.json";
+import weather from "../../assets/weather-dashboard.png";
+import quiz from "../../assets/coding-quiz.png";
 
 export default function ProjectGallery() {
   return (
     <div>
-      <Project title={projects[0].title} repoUrl={projects[0].repoUrl} pageUrl={projects[0].pageUrl}/>
-      <Project title={projects[1].title} repoUrl={projects[1].repoUrl} pageUrl={projects[1].pageUrl}/>
-      <Project title={projects[2].title} repoUrl={projects[2].repoUrl} pageUrl={projects[2].pageUrl}/>
-
+      <Project
+        title={projects[0].title}
+        repoUrl={projects[0].repoUrl}
+        pageUrl={projects[0].pageUrl}
+        image={projects[0].image}
+      />
+      <Project
+        title={projects[1].title}
+        repoUrl={projects[1].repoUrl}
+        pageUrl={projects[1].pageUrl}
+        image={weather}
+      />
+      <Project
+        title={projects[2].title}
+        repoUrl={projects[2].repoUrl}
+        pageUrl={projects[2].pageUrl}
+        image={quiz}
+      />
     </div>
   );
 }
