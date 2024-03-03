aec0424f1bbd98c4b524d3075205615be14268e1
Abdalla Diaai <56742529+abdalla-diaai@users.noreply.github.com>
2024-03-03 16:06:58 +0000

    added styles to cards, description about projects.

--- portfolio/src/components/pages/Project.jsx
+++ portfolio/src/components/pages/Project.jsx
@@ -2,29 +2,30 @@ import React from "react";
 
 export default function Project(props) {
   return (
-    <div className="card">
-      <img className="project-image" alt={props.name} src={props.image} />
-      <div className="card-body">
-        <h5 clasName="card-title">{props.title}</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-        <a
-          className="btn btn-dark btn-sm home-btn"
-          href={props.repoUrl}
-          role="button"
-        >
-          GitHub Repository
-        </a>
-        <a
-          className="btn btn-dark btn-sm home-btn"
-          href={props.pageUrl}
-          role="button"
-        >
-          {props.title} Page
-        </a>
+      <div className="row">
+        <div className="card text-center">
+          <img className="project-image" alt={props.name} src={props.image} />
+          <div className="card-body">
+            <h5 className="card-title">{props.title}</h5>
+            <p className="card-text">
+              {props.description}
+            </p>
+            <a
+              className="btn btn-dark btn-sm home-btn"
+              href={props.repoUrl}
+              role="button"
+            >
+              GitHub Repository
+            </a>
+            <a
+              className="btn btn-secondary btn-sm home-btn"
+              href={props.pageUrl}
+              role="button"
+            >
+              {props.title} Page
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
   );
 }
--- portfolio/src/components/pages/ProjectGallery.jsx
+++ portfolio/src/components/pages/ProjectGallery.jsx
@@ -6,21 +6,24 @@ import quiz from "../../assets/coding-quiz.png";
 
 export default function ProjectGallery() {
   return (
-    <div>
+    <div className="container" id="projects-container">
       <Project
         title={projects[0].title}
+        description={projects[0].description}
         repoUrl={projects[0].repoUrl}
         pageUrl={projects[0].pageUrl}
         image={projects[0].image}
       />
       <Project
         title={projects[1].title}
+        description={projects[1].description}
         repoUrl={projects[1].repoUrl}
         pageUrl={projects[1].pageUrl}
         image={weather}
       />
       <Project
         title={projects[2].title}
+        description={projects[2].description}
         repoUrl={projects[2].repoUrl}
         pageUrl={projects[2].pageUrl}
         image={quiz}
