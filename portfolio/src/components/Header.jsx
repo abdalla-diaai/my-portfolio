6384f0f8b7a3b823a4cc61c624ac0ef3692ad6fa
Abdalla Diaai <56742529+abdalla-diaai@users.noreply.github.com>
2024-03-03 11:50:22 +0000

    added logo to navbar.

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -3,12 +3,12 @@ import { NavLink } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-
+import logo from "../assets/letter-a.png"
 export default function Header() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">ABDALLA</Navbar.Brand>
+        <Navbar.Brand href="#home"> <img src={logo} width="30" height="30" className="d-inline-block align-top" alt="" /></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
@@ -28,14 +28,6 @@ export default function Header() {
               }
             >
               Project Gallery
-            </NavLink>
-            <NavLink
-              to="project"
-              className={({ isActive }) =>
-                isActive ? "nav-link active" : "nav-link"
-              }
-            >
-              Project
             </NavLink>
             <NavLink
               to="contact"
