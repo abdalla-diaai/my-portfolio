6ad48ec884f800245e3f321b7253755448b133a5
Abdalla Diaai <56742529+abdalla-diaai@users.noreply.github.com>
2024-03-05 17:02:50 +0000

    created project gallery separate.

--- src/components/pages/ProjectGallery.jsx
+++ src/components/pages/ProjectGallery.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Project from "./Project";
 import projects from "../utils/projects.json";
 import Footer from "../Footer";
+import weather from "../../assets/weather.gif"
+import quiz from "../../assets/coding-quiz.gif"
 
 export default function ProjectGallery() {
   return (
     <div className="container" id="projects-container">
       <h3 id="project-heading">Project Examples</h3>
-      {projects.map((project) => (
+      {/* this approach did not work on deployment */}
+      {/* {projects.map((project) => (
         <Project
           title={project.title}
           image={project.image}
@@ -15,7 +18,11 @@ export default function ProjectGallery() {
           repoUrl={project.repoUrl}
           pageUrl={project.pageUrl}
         />
-      ))}
+      ))} */}
+
+      <Project title={projects[0].title} description={projects[0].description} image={projects[0].image} repoUrl={projects[0].repoUrl} pageUrl={projects[0].pageUrl}/>
+      <Project title={projects[1].title} description={projects[1].description} image={weather} repoUrl={projects[1].repoUrl} pageUrl={projects[1].pageUrl}/>
+      <Project title={projects[2].title} description={projects[2].description} image={quiz} repoUrl={projects[2].repoUrl} pageUrl={projects[2].pageUrl}/>
       <Footer />
     </div>
   );
