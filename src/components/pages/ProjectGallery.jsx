dce569897b6752fbb7f5d002abf040fbaddf3580
Abdalla Diaai <56742529+abdalla-diaai@users.noreply.github.com>
2024-03-10 13:18:28 +0000

    moved gifs to public folder.

--- src/components/pages/ProjectGallery.jsx
+++ src/components/pages/ProjectGallery.jsx
@@ -2,27 +2,28 @@ import React from "react";
 import Project from "./Project";
 import projects from "../utils/projects.json";
 import Footer from "../Footer";
-import weather from "../../assets/weather.gif"
-import quiz from "../../assets/coding-quiz.gif"
+import weather from "../../assets/weather.gif";
+import quiz from "../../assets/coding-quiz.gif";
 
 export default function ProjectGallery() {
   return (
     <div className="container" id="projects-container">
       <h3 id="project-heading">Project Examples</h3>
       {/* this approach did not work on deployment */}
-      {/* {projects.map((project) => (
+      {projects.map((project) => (
         <Project
+          key={project.title}
           title={project.title}
           image={project.image}
           description={project.description}
           repoUrl={project.repoUrl}
           pageUrl={project.pageUrl}
         />
-      ))} */}
+      ))}
 
-      <Project title={projects[0].title} description={projects[0].description} image={projects[0].image} repoUrl={projects[0].repoUrl} pageUrl={projects[0].pageUrl}/>
+      {/* <Project title={projects[0].title} description={projects[0].description} image={projects[0].image} repoUrl={projects[0].repoUrl} pageUrl={projects[0].pageUrl}/>
       <Project title={projects[1].title} description={projects[1].description} image={weather} repoUrl={projects[1].repoUrl} pageUrl={projects[1].pageUrl}/>
-      <Project title={projects[2].title} description={projects[2].description} image={quiz} repoUrl={projects[2].repoUrl} pageUrl={projects[2].pageUrl}/>
+      <Project title={projects[2].title} description={projects[2].description} image={quiz} repoUrl={projects[2].repoUrl} pageUrl={projects[2].pageUrl}/> */}
       <Footer />
     </div>
   );
