e0bcb67d5be42bd251db5227d1269a154ab31bfb
Abdalla Diaai <56742529+abdalla-diaai@users.noreply.github.com>
2024-03-14 10:46:41 +0000

    testing path to images.

--- src/components/pages/ProjectGallery.jsx
+++ src/components/pages/ProjectGallery.jsx
@@ -10,7 +10,7 @@ export default function ProjectGallery() {
     <div className="container" id="projects-container">
       <h3 id="project-heading">Project Examples</h3>
       {/* this approach did not work on deployment */}
-      {projects.map((project) => (
+      {/* {projects.map((project) => (
         <Project
           key={project.title}
           title={project.title}
@@ -19,11 +19,11 @@ export default function ProjectGallery() {
           repoUrl={project.repoUrl}
           pageUrl={project.pageUrl}
         />
-      ))}
+      ))} */}
 
-      {/* <Project title={projects[0].title} description={projects[0].description} image={projects[0].image} repoUrl={projects[0].repoUrl} pageUrl={projects[0].pageUrl}/>
+      <Project title={projects[0].title} description={projects[0].description} image={projects[0].image} repoUrl={projects[0].repoUrl} pageUrl={projects[0].pageUrl}/>
       <Project title={projects[1].title} description={projects[1].description} image={weather} repoUrl={projects[1].repoUrl} pageUrl={projects[1].pageUrl}/>
-      <Project title={projects[2].title} description={projects[2].description} image={quiz} repoUrl={projects[2].repoUrl} pageUrl={projects[2].pageUrl}/> */}
+      <Project title={projects[2].title} description={projects[2].description} image="/coding-quiz.gif" repoUrl={projects[2].repoUrl} pageUrl={projects[2].pageUrl}/>
       <Footer />
     </div>
   );
